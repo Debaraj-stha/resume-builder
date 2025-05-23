c8696da09502b5c1dda58ebd89920ca127cb3189
Debaraj-stha <dstha221@gmail.com>
2025-05-23 14:25:08 +0545

    making  wrapper of different layout chooser holder scrollable

--- src/components/DifferentLayoutHolder.jsx
+++ src/components/DifferentLayoutHolder.jsx
@@ -1,9 +1,10 @@
 import { memo, useEffect, useState } from "react";
-import styled, { keyframes, css } from "styled-components";
+import styled, { keyframes, css, useTheme } from "styled-components";
 import { all_layouts_image_map } from "./LayoutImages";
 import { GridTwo } from "./layouts/input-layout/GridCards";
-import { H3 } from "./CustomComponents";
+import { H2, H3, Hspace } from "./CustomComponents";
 import { useNavigate } from "react-router-dom";
+import { LineDivider } from "./Divider/TransparentDividers";
 
 const slideIn = keyframes`
   from {
@@ -31,20 +32,23 @@ const Wrapper = styled.div.withConfig({ shouldForwardProp: (prop) => !['isOpen',
   width: 100%;
   max-width: 500px;
   background: ${({ backgroundColor, theme }) =>
-        backgroundColor || theme.colors.background || "#222"};
+    backgroundColor || theme.colors.background || "#222"};
   padding: 1rem;
   position: fixed;
   top: 16%;
-  height: 100vh;
-  overflow-y: auto;
   z-index: 999;
   box-shadow: -8px 0 15px rgba(0, 0, 0, 0.25), 0 8px 15px rgba(0, 0, 0, 0.25);
   animation: ${({ isOpen }) =>
-        isOpen
-            ? css`${slideIn} 0.4s ease-out forwards`
-            : css`${slideOut} 0.4s ease-in forwards`};
+    isOpen
+      ? css`${slideIn} 0.4s ease-out forwards`
+      : css`${slideOut} 0.4s ease-in forwards`};
   pointer-events: ${({ isOpen }) => (isOpen ? "auto" : "none")};
 `;
+const ScrollableWrapper=styled.div`
+  max-height:100vh;
+  height:70vh;
+  overflow-y: auto;
+`
 
 const ImageWrapper = styled.div`
     display: flex;
@@ -76,44 +80,51 @@ const ImageWrapper = styled.div`
   }
 `;
 
-const DifferentLayoutHolder = memo(({ isOpen,onHide }) => {
-    const [visible, setVisible] = useState(isOpen);
-    const navigate = useNavigate()
-    const handleLayoutClick = (category, index) => {
-        try {
-            navigate(`/build-resume/${category}/${index}`)
-           onHide()
-        } catch (error) {
-            console.log("error while changing layout", error)
-        }
+const DifferentLayoutHolder = memo(({ isOpen, onHide }) => {
+  const [visible, setVisible] = useState(isOpen);
+  const theme = useTheme()
+  const navigate = useNavigate()
+  const handleLayoutClick = (category, index) => {
+    try {
+      navigate(`/build-resume/${category}/${index}`)
+      onHide()
+    } catch (error) {
+      console.log("error while changing layout", error)
     }
-    useEffect(() => {
-        if (isOpen) {
-            setVisible(true);
-        } else {
-            // delay unmount to allow animation
-            const timeout = setTimeout(() => setVisible(false), 400);
-            return () => clearTimeout(timeout);
-        }
-    }, [isOpen]);
+  }
+  useEffect(() => {
+    if (isOpen) {
+      setVisible(true);
+    } else {
+      // delay unmount to allow animation
+      const timeout = setTimeout(() => setVisible(false), 400);
+      return () => clearTimeout(timeout);
+    }
+  }, [isOpen]);
 
-    if (!visible) return null;
+  if (!visible) return null;
 
-    return (
-        <Wrapper isOpen={isOpen}>
-            <GridTwo>
-                {Object.entries(all_layouts_image_map).flatMap(
-                    ([category, layoutMap], outerIndex) =>
-                        Object.entries(layoutMap).map(([layoutName, imageSRC], index) => (
-                            <ImageWrapper key={`${outerIndex}-${index}`} onClick={() => handleLayoutClick(category, index + 1)}>
-                                <H3>{category} - {layoutName}</H3>
-                                <img src={imageSRC} loading="lazy" alt={`${category} ${layoutName}`} />
-                            </ImageWrapper>
-                        ))
-                )}
-            </GridTwo>
-        </Wrapper>
-    );
+  return (
+    <Wrapper isOpen={isOpen}>
+      <H2>Try Different Resume Layouts</H2>
+      <LineDivider backgroundColor={theme.colors.text} />
+      <Hspace height="10px"/>
+      <ScrollableWrapper>
+        <GridTwo>
+          {Object.entries(all_layouts_image_map).flatMap(
+            ([category, layoutMap], outerIndex) =>
+              Object.entries(layoutMap).map(([layoutName, imageSRC], index) => (
+                <ImageWrapper key={`${outerIndex}-${index}`} onClick={() => handleLayoutClick(category, index + 1)}>
+                  <H3>{category} - {layoutName}</H3>
+                  <img src={imageSRC} loading="lazy" alt={`${category} ${layoutName}`} />
+                </ImageWrapper>
+              ))
+          )}
+        </GridTwo>
+      </ScrollableWrapper>
+
+    </Wrapper>
+  );
 })
 
 export default DifferentLayoutHolder;
