91cd53b55523a63c19d6ad9ccd8ba5293975ba8b
Debaraj-stha <dstha221@gmail.com>
2025-05-24 16:03:06 +0545

    fixing bugs

--- main.py
+++ main.py
@@ -14,13 +14,18 @@ app.add_middleware(
 )
 
 nlp = spacy.load("en_core_web_sm")
-
 SECTION_HEADERS = {
     "education": ["education", "academic background", "qualifications"],
     "experience": ["experience", "work experience", "employment", "professional experience"],
     "skills": ["skills", "technical skills","key skills","industry skills"],
     "certifications": ["certifications", "certificates"],
     "projects": ["projects", "personal projects"],
+    "achievements":["key achievements","achievements","achievement","most proud of"],
+    "trainings":["trainings","training"],
+    "awards":["awards","honors"],
+    "industry_expertise":["industry expertise"],
+    "passions":["passions","passions"],
+    "languages":["language"]
 }
 
 def extract_text_from_pdf(pdf_path):
@@ -29,6 +34,24 @@ def extract_text_from_pdf(pdf_path):
         for page in pdf.pages:
             text += page.extract_text() + "\n"
     return text
+def extract_liks(text):
+    url_pattern = re.compile(r'(https?://[^\s]+)')
+    urls=url_pattern.findall(text.lower())
+    links = {
+        "linkedin": None,
+        "github": None,
+        "portfolio": None,
+    }
+    for url in urls:
+        if "linkedin.com" in url:
+            links["linkedin"] = url
+        elif "github.com" in url:
+            links["github"] = url
+        elif any(domain in url for domain in ["portfolio", "personal", "site", "dev", "mywebsite", "me"]):
+            links["portfolio"] = url
+        elif links["portfolio"] is None and "linkedin.com" not in url and "github.com" not in url:
+            links["portfolio"] = url
+    return links
 
 def extract_name(text):
     doc = nlp(text[:300])
@@ -59,19 +82,29 @@ def extract_section(text, section_name):
             break
         if capture:
             content.append(line.strip())
-    return "\n".join(content).strip()
+    return content
 
 def extract_resume_data(pdf_path):
     text = extract_text_from_pdf(pdf_path)
     data = {
-        "name": extract_name(text),
-        "email": extract_contact_info(text)["email"],
-        "phone": extract_contact_info(text)["phone"],
+  
+        "personalDetails":{
+                "name": extract_name(text),
+                "email": extract_contact_info(text)["email"],
+                "phone": extract_contact_info(text)["phone"],
+                "urls":extract_liks(text)
+        },
         "education": extract_section(text, "education"),
         "experience": extract_section(text, "experience"),
         "skills": extract_section(text, "skills"),
         "certifications": extract_section(text, "certifications"),
         "projects": extract_section(text, "projects"),
+        "trainings":extract_section(text,"trainigs"),
+        "passions":extract_section(text,"passions"),
+        "industry_expertise":extract_section(text,"industry_expertise"),
+        "languages":extract_section(text,"languages"),
+        "awards":extract_section(text,"awards"),
+        "achievements":extract_section(text,"achievements"),
         "raw_text": text,
     }
     return data
